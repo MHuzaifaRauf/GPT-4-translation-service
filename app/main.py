a92139260a9581210749c453d7845fc86551a538
Huzaifa Rauf <huzaifarauf00@gmail.com>
2024-07-07 19:03:13 +0500

    Seperated static file

--- app/main.py
+++ app/main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Setup for templates
 templates = Jinja2Templates(directory='templates')
+# Setup for static files
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 @app.get('/index', response_class=HTMLResponse)
 def index(request: Request):
